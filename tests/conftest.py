4ff20fcb6afb939d250d80fd45794c268f0e4468
Dinara Safina <dinakhai27@gmail.com>
2024-09-07 23:08:26 +0300

    added dotenv and allure-steps

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,16 @@ from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
+from dotenv import load_dotenv
+import os
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
 
 @pytest.fixture(scope='function', autouse=True)
-def setup_browser():
+def setup_browser(load_env):
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = 1080
     browser.config.window_width = 1920
@@ -20,16 +27,21 @@ def setup_browser():
             "enableVideo": True
         }
     }
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
     browser.config.driver = driver
     yield browser
 
     attach.add_screenshot(browser)
-    attach.add_logs(browser)
+    attach.add_logs(browser0
     attach.add_html(browser)
     attach.add_video(browser)
 
--- tests/test_filling_form.py
+++ tests/test_filling_form.py
@@ -1,24 +1,40 @@
 from modules.pages.registration_form_page import RegistrationPage
-
+import allure
 
 def test_filling_form():
     registration_page = RegistrationPage()
+    with allure.step("Open registration form")
+        registration_page.open()
 
-    registration_page.open()
-    registration_page.fill_first_name('Dinara')
-    registration_page.fill_last_name('Safina')
-    registration_page.fill_email('dinatest@bk.ru')
-    registration_page.choose_gender()
-    registration_page.fill_phone_number('9999999999')
-    registration_page.fill_date_of_birth('1999', 'April', '11')
-    registration_page.fill_subjects('Maths')
-    registration_page.fill_hobbies()
-    registration_page.upload_picture('test_image.jpg')
-    registration_page.fill_address('Russia, Ufa, Lenina St., 1')
-    registration_page.select_state('Haryana')
-    registration_page.select_city('Panipat')
-    registration_page.submit()
+    with allure.step("Fill <Name> field")
+        registration_page.fill_first_name('Dinara')
+    with allure.step("Fill <Lastname> field")
+        registration_page.fill_last_name('Safina')
+    with allure.step("Fill <Email> field")
+        registration_page.fill_email('dinatest@bk.ru')
+    with allure.step("Choose <Gender> radio-button")
+        registration_page.choose_gender()
+    with allure.step("Fill <Number> field")
+        registration_page.fill_phone_number('9999999999')
+    with allure.step("Choose Date of birth")
+        registration_page.fill_date_of_birth('1999', 'April', '11')
+    with allure.step("Fill <Subjects> field")
+        registration_page.fill_subjects('Maths')
+    with allure.step("Choose Hobbies checkbox")
+        registration_page.fill_hobbies()
+    with allure.step("Upload user photo")
+        registration_page.upload_picture('test_image.jpg')
+    with allure.step("Fill <Address> field")
+        registration_page.fill_address('Russia, Ufa, Lenina St., 1')
+    with allure.step("Choose State from list")
+        registration_page.select_state('Haryana')
+    with allure.step("Choose City from list")
+        registration_page.select_city('Panipat')
 
-    registration_page.assert_user_submitted_form('Dinara Safina', 'dinatest@bk.ru', 'Female', '9999999999', '11 April,1999', 'Maths', 'Sports, Reading',
+    with allure.step("Submit form")
+        registration_page.submit()
+
+    with allure.step("Check form results")
+        registration_page.assert_user_submitted_form('Dinara Safina', 'dinatest@bk.ru', 'Female', '9999999999', '11 April,1999', 'Maths', 'Sports, Reading',
         'test_image.jpg', 'Russia, Ufa, Lenina St., 1', 'Haryana Panipat')
 
